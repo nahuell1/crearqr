e1feabf5afb4e0a3b809b3329ba0a6bf2c86c16a
nahuell1 <61886552+nahuell1@users.noreply.github.com>
2025-05-20 23:58:29 -0300

    feat: added vercel analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { GeistSans } from 'geist/font/sans';
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
         className={`${GeistSans.className} antialiased bg-gray-50 dark:bg-gray-900 min-h-screen`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
